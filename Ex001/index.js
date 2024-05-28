4b1362f5d636af94367cb7548b30c45c200b44df
Vanessa <vs6615302@gmail.com>
2024-05-28 19:15:03 -0300

    Alteração no ex001

--- Ex001/index.js
+++ Ex001/index.js
@@ -1,5 +1,21 @@
-let nome= "Vanessa"
+"use strict"
+
+/* let nome= "Vanessa"
 let sobrenome = "Silva"
 
 console.log(nome)
-console.log(sobrenome)
+console.log(sobrenome) */
+
+function teste(){
+  var nome = "Vanessa"
+
+  if(true){
+  console.log("Dentro do IF do teste: " = nome)
+  }
+
+  console.log("Dentro do teste: " + nome)
+}
+
+teste()
+
+console.log("Fora do teste: " + nome)
