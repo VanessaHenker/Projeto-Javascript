ae07b39106a6abb6852afd6d49345901c52e1946
Vanessa <vs6615302@gmail.com>
2024-06-11 19:40:51 -0300

    Alteração no ex007

--- Ex006/index.js
+++ Ex006/index.js
@@ -6,3 +6,16 @@ if (vel > 60) {
 }
 
 console.log('Diriga sempre com um cinto de segurança') */
+
+function calcular(){
+  var txtv = window.document.querySelector('input#txtvel')
+  var res = window.document.querySelector('div#res')
+  var vel = Number(txtv.value)
+  res.innerHTML = `<p> Sua velocidade atual é de <strong>${vel}km/hr</strong></p>`
+
+  if(vel > 60){
+    res.innerHTML += '<p>Você está <strong>multado</strong></p>'
+  }
+
+  res.innerHTML += '</p>Diriga sempre com cinto de segurança</p>'
+}
