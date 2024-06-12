659143a409f34e5b6fd303067fcab4dfa9288155
Vanessa <vs6615302@gmail.com>
2024-06-11 21:52:41 -0300

    Alteração no ex007

--- Ex007/index.js
+++ Ex007/index.js
@@ -1,7 +1,14 @@
 function calcular(){
-  var txtlocal = window.document.querySelector('input#txtl')
+  var txt = window.document.querySelector('input#txtl')
   var res = window.document.querySelector('div#res')
-  
-  res.innerHTML = `<p>Você mora em ${txtlocal}`
+  var vel = String(txt.value)
 
+  res.innerHTML = `<p>Você mora em ${vel}`
+
+  if(vel != 'Brasil' && vel != 'brasil' && vel != "Brazil"){
+    res.innerHTML += '<p>Você é estrangeiro'
+  }
+  else{
+    res.innerHTML += '<p>Você é brasileiro'
+  }
 }
