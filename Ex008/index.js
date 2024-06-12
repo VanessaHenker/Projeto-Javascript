8a49e91d1dd291f9a6d941a18a53697bce5e6320
Vanessa <vs6615302@gmail.com>
2024-06-12 17:35:39 -0300

    Continuação do ex008

--- Ex008/index.js
+++ Ex008/index.js
@@ -1,8 +1,13 @@
-var idade = 19
+var idade = 66
+console.log(`Você tem ${idade} anos`)
+
 if(idade < 16){
   console.log('Não vota')
-}else if(idade < 18){
+}else if(idade < 18 || idade > 65){
   console.log('Voto opcional')
 }else{
   console.log('Voto obrigatório')
 }
+
+//Estrutura while
+
