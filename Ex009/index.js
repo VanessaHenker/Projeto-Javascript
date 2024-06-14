0d39de1cf3e66e228d9b705dadbbf4320763d215
Vanessa <vs6615302@gmail.com>
2024-06-13 23:28:12 -0300

    Alteração no ex009

--- Ex009/index.js
+++ Ex009/index.js
@@ -1,10 +1,20 @@
-console.log('Vai começar..')
-teste = [5]
+/*
+num.push(2) = acrescenta uma posção no vetor
+num.length = quantidade de posições
+num.sort() = organiza os numeros em ordem crescente
 
+num.sort()
+num.push()
+
+*/
+
+
+console.log('Vai começar..')
+var teste = [5]
 for(var i = 0; i < 5; i++){
   teste[i] = i
 }
 
-for(var i = 0;  i < 5; i++){
+for(var i = 0;  i < teste.length; i++){
   console.log(teste[i])
 }
