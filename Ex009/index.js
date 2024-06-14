a138a753c3418d067fcbb35b9e19d690bfe4760d
Vanessa <vs6615302@gmail.com>
2024-06-13 23:36:20 -0300

    Alteração no ex009

--- Ex009/index.js
+++ Ex009/index.js
@@ -11,10 +11,16 @@ num.push()
 
 console.log('Vai começar..')
 var teste = [5]
+//var teste = [5,6,7] - exemplo
+
 for(var i = 0; i < 5; i++){
   teste[i] = i
 }
 
 for(var i = 0;  i < teste.length; i++){
   console.log(teste[i])
+}
+
+for(let i = 0; i < teste.length; i++){
+  console.log(`A posicao ${i} tem o valor ${teste[i]}`)
 }
