a6cb64ed20f22fb29c99505ed866345b04499133
Vanessa <vs6615302@gmail.com>
2024-06-25 16:11:22 -0300

    Alteração no ex010

--- Ex010/script.js
+++ Ex010/script.js
@@ -8,3 +8,26 @@ function parImpar(n){
 } 
 
 console.log(parImpar(6))
+
+function soma(n1 = 0, n2 = 0){
+  return n1 + n2
+}
+
+console.log(soma(2,2))
+
+let v = function(x){
+  return x*2
+}
+
+console.log(v(5))
+
+
+function fatorial(n){
+  let fat = 1
+  for(let i = n; i > 1; i--){
+    fat *= i
+  }
+  return fat
+}
+
+console.log(fatorial(5))
