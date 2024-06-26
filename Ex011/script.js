0d97221d87defad22f6516ca4bf16910ff80c19b
Vanessa <vs6615302@gmail.com>
2024-06-26 18:54:17 -0300

    Alteração no ex011

--- Ex011/script.js
+++ Ex011/script.js
@@ -14,23 +14,16 @@ console.log('Horário atual: ' + currentTime);
 
 var hora = window.document.getElementById('hora-atual')
 
-  // Mudar o src de um elemento img existente
-  document.getElementById('minhaImagem').src = 'image-dia.jpeg';
-
-  // Criar e adicionar um novo elemento img dinamicamente
-  var novaImagem = document.createElement('img');
-  novaImagem.src = 'image-tarde.jpg';
-  novaImagem.alt = 'Outra imagem com JavaScript';
-
-// Adicionar a nova imagem ao container
-document.getElementById('imgDia').appendChild(novaImagem);
   if(hours < 12){
     hora.innerHTML = `Bom dia, agora são ${hours}:${minutes}`
+  document.getElementById('minhaImagem').src = 'image-dia.jpeg';
   }
   if(hours >= 12){
     hora.innerHTML = `Boa tarde, agora são ${hours}:${minutes}`
+    document.getElementById('minhaImagem').src = 'image-tarde.jpg';
  
   }
   if(hours > 17 || hours <= 4){
     hora.innerHTML = `Boa noite, agora são ${hours}:${minutes}`
+    document.getElementById('minhaImagem').src = 'image-noite.jpg';
   }
