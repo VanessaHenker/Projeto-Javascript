8a055bf110d119d0b9ec3bb56d8c3f1cfa1c8a09
Vanessa <vs6615302@gmail.com>
2024-06-26 15:22:50 -0300

    Alteração no ex011

---
+++ Ex011/script.js
@@ -0,0 +1,13 @@
+let now = new Date();
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let seconds = now.getSeconds();
+
+hours = hours < 10 ? '0' + hours : hours;
+minutes = minutes < 10 ? '0' + minutes : minutes;
+seconds = seconds < 10 ? '0' + seconds : seconds;
+
+// Exibe o horário atual no formato HH:MM:SS
+let currentTime = hours + ':' + minutes + ':' + seconds;
+console.log('Horário atual: ' + currentTime);
