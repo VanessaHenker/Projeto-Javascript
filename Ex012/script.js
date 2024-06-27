35fb7bbeb6bb1ef591c50541f02478f6e78075f2
Vanessa <vs6615302@gmail.com>
2024-06-27 16:48:07 -0300

    Alteração no ex012

--- Ex012/script.js
+++ Ex012/script.js
@@ -1,1 +1,14 @@
+function calcular(){
+  var ano = window.document.getElementById('n1')
+  var res = window.document.getElementById('res')
+  var n1 = Number(ano.value)
 
+  var sexo = window.document.getElementById('sexo')
+  var txt = String(sexo.value)
+
+  
+
+
+  var s = 2024 - n1
+  res.innerHTML = `${s} ${txt}`
+}
