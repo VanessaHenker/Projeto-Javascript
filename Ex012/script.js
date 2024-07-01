ecc14aada138683fa7f5b9772f48d8d4e3bee9c7
Vanessa <vs6615302@gmail.com>
2024-07-01 18:12:01 -0300

    Alteração no ex012

--- Ex012/script.js
+++ Ex012/script.js
@@ -4,7 +4,21 @@ function calcular(){
   var n1 = Number(ano.value)
 
   
-
   var s = 2024 - n1
   res.innerHTML = `Você tem ${s} anos`
+}
+
+
+function verificarSexo(sexo) {
+  var teste =  window.document.getElementById('teste')
+
+    if (sexo.toLowerCase() === 'F') {
+        return 'Feminino';
+    } else if (sexo.toLowerCase() === 'M') {
+        return 'Masculino';
+    } else {
+        return 'Sexo não especificado';
+    }
+
+  teste.innerHTML = `Você tem ${s} anos`
 }
