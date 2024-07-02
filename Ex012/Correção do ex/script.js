43dd3cac46959132b5d3c18469668f773e102e63
Vanessa <vs6615302@gmail.com>
2024-07-02 16:21:45 -0300

    Alteração no ex012

--- Ex012/Correção do ex/script.js
+++ Ex012/Correção do ex/script.js
@@ -3,4 +3,8 @@ function verificar(){
   var ano = data.getFullYear()
   var fano = document.getElementById('txtano')
   var res = document.getElementById('res')
+
+  if(fano.value.lenght == 0 || Number(fano.value) > ano){
+    window.alert('Erro, verifique os dados novamente!')
+  }
 }
