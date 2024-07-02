c7bf63c666263b887e9bdf7f79bcf82ec33746d0
Vanessa <vs6615302@gmail.com>
2024-07-02 13:25:16 -0300

    Alteração no ex912

--- Ex012/script.js
+++ Ex012/script.js
@@ -1,12 +1,16 @@
-function calcular(){
+function calcular() {
   var ano = window.document.getElementById('n1')
   var res = window.document.getElementById('res')
   var n1 = Number(ano.value)
 
-  
-  var s = 2024 - n1
- 
-  const radios = document.getElementsByName('sexo');
+  if (n1 > 2024 || n1 < 1900) {
+    alert("Erro, ano inválido!")
+  }
+  else {
+    var s = 2024 - n1
+
+
+    const radios = document.getElementsByName('sexo');
     let sexoSelecionado = '';
     for (const radio of radios) {
       if (radio.checked) {
@@ -15,38 +19,38 @@ function calcular(){
       }
     }
 
-  
-
     if (sexoSelecionado == 'M') {
-      if(s < 18){
+      if (s < 18) {
         res.innerHTML = `Detectamos um menino com ${s} anos`;
         document.getElementById('minhaImagem').src = 'imagem-menino.webp'
       }
-      if(s > 18){
+      if (s > 18) {
         res.innerHTML = `Detectamos um homem com ${s} anos`;
         document.getElementById('minhaImagem').src = 'imagem-homem.avif'
       }
-      if(s >= 60){
+      if (s >= 60) {
         res.innerHTML = `Detectamos um idoso com ${s} anos`;
         document.getElementById('minhaImagem').src = 'homem-idoso.jpg'
       }
-      
+    }
+    else if (sexoSelecionado == 'F') {
+      if (s < 18) {
+        res.innerHTML = `Detectamos uma menina com ${s} anos`;
+        document.getElementById('minhaImagem').src = 'imagem-menina.avif'
+      }
+      if (s > 18) {
+        res.innerHTML = `Detectamos uma mulher com ${s} anos`;
+        document.getElementById('minhaImagem').src = 'imagem-mulher.jpg'
+      }
+      if (s >= 60) {
+        res.innerHTML = `Detectamos uma idosa com ${s} anos`;
+        document.getElementById('minhaImagem').src = 'imagem-idosa.png'
+      }
+
     } 
-    if (sexoSelecionado == 'F') {
-        if(s < 18){
-          res.innerHTML = `Detectamos uma menina com ${s} anos`;
-          document.getElementById('minhaImagem').src = 'imagem-menina.avif'
-        }
-        if(s > 18){
-          res.innerHTML = `Detectamos uma mulher com ${s} anos`;
-          document.getElementById('minhaImagem').src = 'imagem-mulher.jpg'
-        }
-        if(s >= 60){
-          res.innerHTML = `Detectamos uma idosa com ${s} anos`;
-          document.getElementById('minhaImagem').src = 'imagem-idosa.png'
-        }
-       
-    } 
-    
+    else{
+      alert("Erro, sexo não especificado!")
+    }
+  }
 }
 
