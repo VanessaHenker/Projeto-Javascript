33088b67af3bf6fa06996348f2c6003051e006e2
Vanessa <vs6615302@gmail.com>
2024-07-03 18:04:50 -0300

    Alteração no ex012

--- Ex012/Correção do ex/script.js
+++ Ex012/Correção do ex/script.js
@@ -11,17 +11,22 @@ function verificar(){
     var fsex = document.getElementsByName('radsex')
     var idade = ano - Number(fano.value)
     var genero = ''
-    //var img = document.createElement('img')
-    //img.setAttribute('id', 'foto')
+
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    var tam = document.getElementsById('conteudo-principal')
+    tam.classList.add('nova-classe')
+
     if(fsex[0].checked){
       genero = 'Homem'
       if(idade >=0 && idade < 10){
         //criança
 
-        document.getElementById('minhaImagem').src = 'imagem-menino.webp'
-
-        //img.setAttribute('src', 'imagem-menino.webp')
-     
+       // document.getElementById('minhaImagem').src = 'imagem-menino.webp'
+        img.setAttribute('src', 'imagem-menino.webp')
+        img.style.height = '200px'
+        tam.style.height = '300px'
       }
       else if(idade  < 21){
         //jovem
@@ -48,6 +53,6 @@ function verificar(){
     }
     res.style.textAlign = 'center'
     res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-    //res.appendChild(img)
+    res.appendChild(img)
   }
 }
