d95eca546caf6c70360f0fc3ec34873d16afdf5b
Vanessa <vs6615302@gmail.com>
2024-07-04 22:20:12 -0300

    Alteração no ex013

--- Ex013/script.js
+++ Ex013/script.js
@@ -1,26 +1,32 @@
+let i = document.getElementById('inicio')
+let p = document.getElementById('passos')
+let f = document.getElementById('fim')
+let res = document.getElementById('res')
+
+let ini =  2
+let pass = 2
+let fim =  10
+
+ /*  res.innerHTML += `${ini}`
+  res.innerHTML += `${pass}`
+  res.innerHTML += `${fim}` */
+
+  while(ini <= fim){
+    res.innerHTML += ` ${ini} `
+    ini = ini + pass
+  } 
+
+
 function calcular(){
-  let i = document.getElementById('inicio')
-  let p = document.getElementById('passos')
-  let f = document.getElementById('fim')
-  let res = document.getElementById('res')
 
-
-  if(i.value.length == 0 || p.value.length == 0 || f.value.length){
-    res.innerHTML = 'Erro, faltam dados'
-  }
-  else{
-    let ini = Number(i.value)
-    let pass = Number(p.value)
-    let fim = Number(f.value)
+    while(ini <= fim){
+      res.innerHTML += `${ini}`
+      ini = ini + pass
+    } 
   
-
-    for(let cont = ini; cont <= fim; cont += pass){
-      res.innerHTML += `${cont}`
-    }
-  }
 }
 
-var ini = 2
+/* var ini = 2
 var fim = 10
 
 var pass = 2
@@ -30,3 +36,4 @@ while(ini <= fim){
   console.log(ini)
   ini = ini + pass
 } 
+ */
