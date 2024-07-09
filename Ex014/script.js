7c8ebbd90f1cf9fee864791ac134da1287b4dfd1
Vanessa <vs6615302@gmail.com>
2024-07-08 22:00:51 -0300

    Alteração no ex014

--- Ex014/script.js
+++ Ex014/script.js
@@ -1,3 +1,15 @@
 function calcular(){
+  var v = document.getElementById('num')
+  var res = document.getElementById('res')
+
+  var valor = Number(v.value)
+
+  res.innerHTML = "";
+  for(let i = 1; i <= 10; i++){
+     var linha = valor + "x" + i + " = " + (valor * 1)
+     var elementoLinha = document.createElement("p")
+     elementoLinha.textContent = linha
+     res.appendChild(elementoLinha)
   
+  }
 }
