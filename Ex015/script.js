baf36f20383af86b72ce8b84963246ac24f0ff58
Vanessa <vs6615302@gmail.com>
2024-07-19 20:13:52 -0300

    Alteração no ex002

---
+++ Ex015/script.js
@@ -0,0 +1,32 @@
+let v = document.getElementById('num');
+let res = document.getElementById('res');
+let valores = [];
+
+function isNumero(n){
+  if(Number(n) >= 1 && Number(n) <= 100){
+    return true;
+  }
+  else{
+    false;
+  }
+}
+
+function inLista(n, l){
+  if(l.index(Number(n)) != -1){
+    return true;
+  }
+  else{
+    false;
+  }
+}
+
+function calcular(){
+  if(isNumero(num.value) && !inLista(num.value, valores)){
+
+  }
+  else{
+    window.alert('valor inválido')
+  }
+  
+}
+
