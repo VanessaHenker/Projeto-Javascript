1f0ae7198a5f96b5eb9fbde1e2b273c84f92fda4
Vanessa <vs6615302@gmail.com>
2024-07-20 16:22:58 -0300

    Alteração no ex015

--- Ex015/script.js
+++ Ex015/script.js
@@ -1,34 +1,8 @@
 let v = document.getElementById('num');
 let res = document.getElementById('res');
 let fin = document.getElementById('finalizar');
-let add = document.getElementById('adicionar');
 let valores = [];
 
-function isNumero(n){
-  if(Number(n) >= 1 && Number(n) <= 100){
-    return true;
-  }
-  else{
-    false;
-  }
-}
-
-function inLista(n, l){
-  if(l.index(Number(n)) != -1){
-    return true;
-  }
-  else{
-    false;
-  }
-}
-
-function calcular(){
-  if(isNumero(num.value) && !inLista(num.value, valores)){
-
-  }
-  else{
-    window.alert('valor inválido')
-  }
+function adicionar(){
   
 }
-
