cd0158daecc2f0f583e078d767696c4c201cd043
Vanessa <vs6615302@gmail.com>
2024-08-26 21:14:14 -0300

    Alteração no cod

--- Projeto react/src/App.js
+++ Projeto react/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Vanessa'
+
   return (
     <div className = "App">
       <h1>Olá react</h1>
+      <p>teste</p>
+      <p>Olá, {name}</p>
     </div>
-  );
-}
+  )
 
+}
 export default App;
