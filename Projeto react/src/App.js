9a21d4741778a5bd8321ae3803f87221c2d2116c
Vanessa <vs6615302@gmail.com>
2024-08-26 21:22:59 -0300

    Ultima alteração

--- Projeto react/src/App.js
+++ Projeto react/src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 
 function App() {
   const name = 'Vanessa'
+  const newName = name.toLocaleUpperCase()
 
+  function soma(a, b){
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
   return (
     <div className = "App">
       <h1>Olá react</h1>
       <p>teste</p>
-      <p>Olá, {name}</p>
+      <p>Olá,{newName}</p>
+      <p>Soma: {soma(1, 2)}</p>
+      <img scr= {url} alt = 'minha imagem'></img>
     </div>
   )
 
