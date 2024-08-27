552cb259be3f9dca026375b8642bfb8ab462e8c1
Vanessa <vs6615302@gmail.com>
2024-08-27 20:01:15 -0300

    Alteração nos arquivos

--- Projeto react/src/App.js
+++ Projeto react/src/App.js
@@ -1,23 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = 'Vanessa'
-  const newName = name.toLocaleUpperCase()
-
-  function soma(a, b){
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
   return (
-    <div className = "App">
-      <h1>Olá react</h1>
-      <p>teste</p>
-      <p>Olá,{newName}</p>
-      <p>Soma: {soma(1, 2)}</p>
-      <img scr= {url} alt = 'minha imagem'></img>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
-
+  );
 }
+
 export default App;
